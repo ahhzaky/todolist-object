7244f3294dc67b4eb4bfecccee0987357290d50b
ahhzaky <siahmadzaky@gmail.com>
2021-02-12 23:58:01 +0700

    test Menghapus todolist logic

--- src/test/service/TodoListServiceTest.java
+++ src/test/service/TodoListServiceTest.java
@@ -8,7 +8,7 @@ import service.TodoListServiceImpl;
 
 public class TodoListServiceTest {
     public static void main(String[] args) {
-        testAddTodoList();
+        testRemoveTodoList();
     }
 
     public static void testShowTodoList(){
@@ -33,5 +33,21 @@ public class TodoListServiceTest {
         todoListService.addTodoList("Belajar Golang v2");
 
         todoListService.showTodoList();
+    }
+
+    public static void testRemoveTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+
+        todoListService.addTodoList("Belajar Java v2");
+        todoListService.addTodoList("Belajar Python v2");
+        todoListService.addTodoList("Belajar Golang v2");
+
+        todoListService.showTodoList();
+
+        todoListService.removeTodoList(2);
+
+        todoListService.showTodoList();
+
     }
 }
