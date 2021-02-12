f638bf6b94f0349068fc916e52081154ee3bd9c2
ahhzaky <siahmadzaky@gmail.com>
2021-02-13 00:03:56 +0700

    View: Menampilkan TodoList

--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -1,12 +1,42 @@
 package view;
 
+import service.TodoListService;
+import util.InpuUtil;
+
 public class TodoListView {
+
+    private TodoListService todoListService;
+
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
 
     /**
      * menampilkan todo list
      */
     public void showTodoList(){
+        while (true) {
+            todoListService.showTodoList();
 
+            System.out.println("MENU : ");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+
+            // input dari user
+            var input = InpuUtil.input("Pilih");
+
+            if (input.equals("1")){
+                addTodoList();
+            } else if (input.equals("2")){
+                removeTodoList();
+            } else if (input.equals("x")){
+                break;
+            }
+            else {
+                System.out.println("Pilihan tidak ada");
+            }
+        }
     }
 
     /**
