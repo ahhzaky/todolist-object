d54bc4bd3d3280dae993d1f9bb2a3d2bb3fc47ee
ahhzaky <siahmadzaky@gmail.com>
2021-02-12 23:12:17 +0700

    Membuat View

---
+++ src/view/TodoListView.java
@@ -0,0 +1,25 @@
+package view;
+
+public class TodoListView {
+
+    /**
+     * menampilkan todo list
+     */
+    public void showTodoList(){
+
+    }
+
+    /**
+     * menambahkan Todo ke list
+     */
+    public void addTodoList(){
+
+    }
+
+    /**
+     * hapus todo dari list
+     */
+    public void  removeTodoList(){
+
+    }
+}
